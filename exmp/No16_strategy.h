184dd18c6cfac78afdafc76baee3ad64ff28fcf0
FlyingMammuthus <40666168+FlyingMammuthus@users.noreply.github.com>
2019-03-04 13:07:06 -0500

    Update No16_strategy.h

--- exmp/No16_strategy.h
+++ exmp/No16_strategy.h
@@ -2,17 +2,20 @@
 #include <iostream>
 
 /*
-����ģʽ(Strategy Pattern)������һϵ���㷨����ÿһ���㷨��װ�������������ǿ����໥�滻������ģʽ���㷨������ʹ�����Ŀͻ����仯��Ҳ��Ϊ����ģʽ(Policy)��
+策略模式(Strategy Pattern)：定义一系列算法，将每一个算法封装起来，并让它们可以相互替换。策略模式让算法独立于使用它的客户而变化，也称为政策模式(Policy)。
 
-����ʾ���������������㷨�п�������ð�����򣬹鲢����ѡ������ȣ����������Ҫ��һ���㷨�����ṩ��Щ�㷨��һ�������Ľ���������������ж�����������ÿ����������һ�־���������㷨��Ȼ��ʹ�� if...else...ȥ�жϵ����������㷨������ֱ�ӵ���ĳ�����巽�������ַ����ǽ��㷨��ʵ��Ӳ���뵽���У����������ı׶˾����㷨����ǳ�ӷ�ף����ҵ���Ҫ���ӻ��߸���һ���µ����򷽷�ʱ����Ҫ�޸��㷨��Ĵ��룬ͬʱҲ��Ҫ�޸Ŀͻ��˵��ô��Ĵ��롣����ģʽ����Ϊ�˽�������������Ƶġ�
+常见示例：常见的排序算法有快速排序，冒泡排序，归并排序，选择排序等，如果我们需要在一个算法类中提供这些算法，一个常见的解决方法就是在类中定义多个方法，
+每个方法定义一种具体的排序算法，然后使用 if...else...去判断到底是哪种算法，或者直接调用某个具体方法。这种方法是将算法的实现硬编码到类中，
+这样做最大的弊端就是算法类类非常臃肿，而且当需要增加或者更换一种新的排序方法时候，需要修改算法类的代码，同时也需要修改客户端调用处的代码。
+策略模式就是为了解决这列问题而设计的。
 
-��ɫ
+角色
 
-Context: �����࣬ʹ��һ��ConcreteStrategy���������ã�ά��һ����Stategy��������ã�ͬʱ�����Զ���һ���ӿ�����Stategy�����������ݡ�
+Context: 环境类，使用一个ConcreteStrategy对象来配置；维护一个对Stategy对象的引用，同时，可以定义一个接口来让Stategy访问它的数据。
 
-Strategy: ��������࣬��������֧�ֵ��㷨�Ĺ����ӿڡ�Contextʹ������ӿ�������ĳConcreteStrategy������㷨��
+Strategy: 抽象策略类，定义所有支持的算法的公共接口。Context使用这个接口来调用某ConcreteStrategy定义的算法；
 
-ConcreteStrategy: ��������࣬ʵ�� Strategy �ӿڵľ����㷨��
+ConcreteStrategy: 具体策略类，实现 Strategy 接口的具体算法。
 */
 using namespace std;
 
